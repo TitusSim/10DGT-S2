cd24660aa5395266563429370728a85fc2ce64a6
TitusSim <titus.sim@kingsway.school.nz>
2024-09-20 14:50:04 +1200

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,29 @@
+# Showing how variables work
+# Author: Juergen Lier
+# Date: 20 Spetember 2024
+# Version: 1 
+
+
+# Diffrent types of variables
+# Store a number
+print('Check out this number!')
+num_69 = 420
+print(num_69)
+
+# Store a string
+name = "Titus Sim"
+print(name)
+name = "Skibidi Toilet"
+print(name)
+print('Trystan is super duper duper weak \nSIKE THATS THE WRONG NUMBER')
+print()
+
+# Do calculations in a variable and store the result
+sum=69+69 # NOT GOOD DONT DO THIS
+sum = " 
+69 + 69 = 138" # very good, this follows PEP8 Python conventions
+print(sum)
+
+# Adding strings together is called a concatentation
+Concatentation = name + sum 
+print(Concatentation)
