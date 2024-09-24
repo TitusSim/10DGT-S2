520a39f05c5ca5e861f0a13f2847848cf1091a19
TitusSim <titus.sim@kingsway.school.nz>
2024-09-25 10:24:26 +1200

    Add files via upload
    25/09/2024
    
    In here, I learn how to ask the user for their name using 'user input'
    I also learn how to ask for two numbers and sum them together.

---
+++ Loops and Indents V.2.py
@@ -0,0 +1,23 @@
+# Loops and Indents 
+# Author: Titus Sim
+# Date: 25 September 2024
+# Version: 2
+# TODO
+    # Get user input (ask for name)
+    # Get the user to input two number
+    # add the two numbers together
+
+
+# Ask the user for their name
+name = input("What is your name?")
+print(f"Greetings {name}.") # f stands for format
+
+# ask the user for two numbers
+num_1 = int(input("What is your favourite number?"))
+num_2 = int(input("Now, what is your LEAST favourite number??"))
+
+# Add Numbers Together
+sum = num_1 + num_2
+print(f"Here is your two numbers added together, {sum}")
+
+print("YOUR WELCOME BUDDY")
