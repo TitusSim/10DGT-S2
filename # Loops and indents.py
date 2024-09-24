006203b3a8b664c0ba4262d40ee7393059971801
TitusSim <titus.sim@kingsway.school.nz>
2024-09-25 09:56:53 +1200

    Add files via upload

---
+++ # Loops and indents.py
@@ -0,0 +1,18 @@
+# Loops and indents 
+# Author: Juergen Lier
+# Date: 20 September 2024
+# Version 1
+
+
+# for loops. Repeat for a set number of times
+for name_of_lopp in range(5):
+    print("ha  ")    
+# While in loop, Runs until a condition is met
+
+keepgoing_ = "" #Empty variable
+
+while keepgoing_ == "":
+    print('STILL LOOPING')
+    print('yeah, still looping')
+    keep_going = input("Again? Or press any other key to quit")
+
